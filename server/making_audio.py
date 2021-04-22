30dd55867237d95a7b6e25292edab9af632f1d4c
satyarajsinh <sjadeja790@gmail.com>
2021-04-22 10:55:24 +0530

    both project connected

---
+++ server/making_audio.py
@@ -0,0 +1,20 @@
+from gtts import gTTS
+# mytext1 = "building has only four floor. please tell me right floor"
+# languages = "en"
+# myobj = gTTS(text=mytext1,lang=languages,slow=False)
+# myobj.save("notfloor.mp3")
+
+
+# mytext2 = "tell me on which floor you want to go?"
+# lang2 = "en"
+# myobj1 = gTTS(text=mytext2, lang=lang2,slow=False)
+# myobj1.save("gotofloor.mp3")
+
+# mytext3 = "Have a nice day"
+# myobj2 = gTTS(text=mytext3,lang=lang2,slow=False)
+# myobj2.save("thank.mp3")
+
+mytext4 = "Please wear mask"
+myobj4 = gTTS(text=mytext4,lang='en',slow=False)
+myobj4.save("request.mp3")
+
